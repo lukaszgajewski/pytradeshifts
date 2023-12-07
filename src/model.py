e60611718307273ec8ea0aa25f0e7b3cc6fa9ae2
Florian Ulrich Jehn <florianjehn@posteo.de>
2023-12-07 13:49:44 +0100

    Updated docstrings

--- src/model.py
+++ src/model.py
@@ -30,7 +30,11 @@ class PyTradeShifts:
         )
         self.trade_matrix = self.build_trade_matrix()
         
-    def load_data(self, crop, base_year):
+    def load_data(
+            self,
+            crop: str,
+            base_year: int,
+    ) -> tuple[pd.DataFrame, pd.Series]:
         """
         Loads the data into a pandas dataframe and cleans it
         of countries with trade below a certain percentile.
@@ -66,9 +70,13 @@ class PyTradeShifts:
 
         print(f"Loaded data for {crop} in {base_year}.")
 
-        return crop_trade_data, production_data_data
+        return (crop_trade_data, production_data_data)
 
-    def remove_above_percentile(self, trade_matrix, percentile):
+    def remove_above_percentile(
+            self,
+            trade_matrix: pd.DataFrame,
+            percentile: float = 0.75
+    ) -> pd.DataFrame:
         """
         Removes countries with trade below a certain percentile.
 
@@ -94,35 +102,10 @@ class PyTradeShifts:
 
         return trade_matrix
 
-    def build_trade_matrix(self):
-        """
-        Builds the trade matrix for the given crop and base_year.
-
-        Arguments:
-            None
-
-        Returns:
-            pd.DataFrame: The trade matrix.
-        """
-        # Pivot the DataFrame to get a matrix representation
-        trade_matrix = self.trade_data.pivot_table(
-            index='Reporter Countries', columns='Partner Countries', values='Quantity', fill_value=0
-        )
-
-        # Ensure the trade_matrix is quadratic
-        trade_matrix = trade_matrix.add(trade_matrix.transpose(), fill_value=0)
-        trade_matrix = trade_matrix.reindex(index=trade_matrix.columns, columns=trade_matrix.index, fill_value=0)
-
-        # Replace nan values with 0
-        trade_matrix = trade_matrix.fillna(0)
-        print("Finished building the trade matrix.")
-
-        return trade_matrix
-
     def remove_re_exports(self):
         """
         Removes re-exports from the trade matrix.
-        This is a Python implementation of the Matlab code from:
+        This is a Python implementation of the R/Matlab code from:
         Croft, S. A., West, C. D., & Green, J. M. H. (2018).
         "Capturing the heterogeneity of sub-national production
         in global trade flows."
@@ -131,8 +114,7 @@ class PyTradeShifts:
 
         https://doi.org/10.1016/j.jclepro.2018.08.267
 
-        In addition to the original Matlab code, this implementation
-        also includes pre-balancing to ensure that countries don't
+        This implementation also includes pre-balancing to ensure that countries don't
         export more than they produce and import.
 
         Arguments:
@@ -162,7 +144,24 @@ class PyTradeShifts:
         trade_matrix = self.remove_above_percentile(trade_matrix, self.percentile)
         self.trade_matrix = trade_matrix
         
-    def prebalance(self, production_data, trade_matrix, precision=10**-3):
+    def prebalance(
+            self,
+            production_data: pd.Series,
+            trade_matrix: pd.DataFrame,
+            precision=10**-3
+    ) -> pd.DataFrame:
+        """
+        Return prebalaced trading data.
+
+        Arguments:
+            production_data (pd.Series): Vector of production data.
+            trade_matrix (pd.DataFrame): Trade matrix of the crop specified
+            precision (float, optional): Specifies precision of the prebalancing.
+
+        Returns:
+            pandas.DataFrame: Prebalanced trade matrix.
+        """
+        # this is virtually 1:1 as in Croft et al.
         test = production_data + trade_matrix.sum(axis=0) - trade_matrix.sum(axis=1)
         while (test <= -precision).any():
             sf = (production_data + trade_matrix.sum(axis=0)) / trade_matrix.sum(axis=1)
@@ -175,20 +174,53 @@ class PyTradeShifts:
             test = production_data + trade_matrix.sum(axis=0) - trade_matrix.sum(axis=1)
         return trade_matrix
 
-    def remove_net_zero_countries(self, production_data, trade_matrix):
+    def remove_net_zero_countries(
+            self,
+            production_data: pd.Series,
+            trade_matrix: pd.DataFrame,
+    ) -> tuple[pd.Series, pd.DataFrame]:
+        """
+        Return production and trading data with "all zero" countries removed.
+        "All zero" countries are such states that has production = 0 and sum of rows
+        in trade matrix = 0, and sum of columns = 0.
+
+        Arguments:
+            production_data (pd.Series): Vector of production data.
+            trade_matrix (pd.DataFrame): Trade matrix of the crop specified
+
+        Returns:
+            tuple[pd.Series, pd.DataFrame]: Production data and trade matrix
+            without "all zero" countries.
+        """
+        # b_ signifies boolean here, these are filtering masks
         b_zero_prod = production_data == 0
         b_zero_colsum = trade_matrix.sum(axis=0) == 0
         b_zero_rowsum = trade_matrix.sum(axis=1) == 0
         b_filter = ~(b_zero_prod & b_zero_rowsum & b_zero_colsum)
         return production_data[b_filter], trade_matrix.loc[b_filter, b_filter]
 
-    def correct_reexports(self, production_data, trade_matrix):
+    def correct_reexports(
+            self,
+            production_data: pd.Series,
+            trade_matrix: pd.DataFrame,
+    ) -> pd.DataFrame:
         """
-        I know that the variable names here are confusing, but this is a conversion
-        by the original R code from Johanna Hedlung. The variable names are the
-        same as in the R code and we leave them this way, so we can more easily
-        compare the two pieces of code if something goes wrong.
+        Return trading data after correcting for re-exports.
+
+        Input to this function should be prebalanced and have countries with all zeroes
+        removed.
+
+        Arguments:
+            production_data (pd.Series): Vector of production data.
+            trade_matrix (pd.DataFrame): Trade matrix of the crop specified
+        
+        Returns:
+            pd.DataFrame: Trade matrix without re-exports.
         """
+        # I know that the variable names here are confusing, but this is a conversion
+        # by the original R code from Johanna Hedlung/Croft et al.. The variable names are the
+        # same as in the R code and we leave them this way, so we can more easily
+        # compare the two pieces of code if something goes wrong.
         trade_matrix = trade_matrix.T
         production_data = production_data.fillna(0)
 
@@ -199,7 +231,7 @@ class PyTradeShifts:
         c = np.diag(y @ (x - trade_matrix.sum(axis=0)))
         R = (c @ R).T
         R[~np.isfinite(R)] = 0
+        # we could consider parametrising this but Croft had this hard coded
         R[R < 0.001] = 0
 
         return pd.DataFrame(R, index=trade_matrix.index, columns=trade_matrix.columns)
-
