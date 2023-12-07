929fff9db9d0f7302284730f4eb95adc327ab559
Florian Ulrich Jehn <florianjehn@posteo.de>
2023-12-07 11:21:32 +0100

    Trying to unify mine and Lukasz's code

--- src/model.py
+++ src/model.py
@@ -29,7 +29,7 @@ class PyTradeShifts:
             self.base_year,
         )
         self.trade_matrix = self.build_trade_matrix()
-
+        
     def load_data(self, crop, base_year):
         """
         Loads the data into a pandas dataframe and cleans it
@@ -64,6 +64,8 @@ class PyTradeShifts:
         production_data_data.index = production_data_data["Area"]
         production_data_data = production_data_data["Production"]
 
+        print(f"Loaded data for {crop} in {base_year}.")
+
         return crop_trade_data, production_data_data
 
     def remove_above_percentile(self, trade_matrix, percentile):
@@ -88,6 +90,8 @@ class PyTradeShifts:
         trade_matrix = trade_matrix.loc[trade_matrix.sum(axis=1) > 0, :]
         trade_matrix = trade_matrix.loc[:, trade_matrix.sum(axis=0) > 0]
 
+        print(f"Removed countries with trade below the {percentile*100}th percentile.")
+
         return trade_matrix
 
     def build_trade_matrix(self):
@@ -100,22 +104,22 @@ class PyTradeShifts:
         Returns:
             pd.DataFrame: The trade matrix.
         """
-        # Build the trade matrix
+        # Pivot the DataFrame to get a matrix representation
         trade_matrix = self.trade_data.pivot_table(
-            index="Reporter Countries",
-            columns="Partner Countries",
-            values="Quantity",
-            aggfunc="sum",
-            fill_value=0
+            index='Reporter Countries', columns='Partner Countries', values='Quantity', fill_value=0
         )
 
-        # Make it a quadratic matrix
-        index = trade_matrix.index.union(trade_matrix.columns)
-        trade_matrix = trade_matrix.reindex(index=index, columns=index, fill_value=0)
+        # Ensure the trade_matrix is quadratic
+        trade_matrix = trade_matrix.add(trade_matrix.transpose(), fill_value=0)
+        trade_matrix = trade_matrix.reindex(index=trade_matrix.columns, columns=trade_matrix.index, fill_value=0)
+
+        # Replace nan values with 0
+        trade_matrix = trade_matrix.fillna(0)
+        print("Finished building the trade matrix.")
 
         return trade_matrix
 
-    def remove_re_exports(self, percentile=0.75):
+    def remove_re_exports(self):
         """
         Removes re-exports from the trade matrix.
         This is a Python implementation of the Matlab code from:
@@ -127,49 +131,75 @@ class PyTradeShifts:
 
         https://doi.org/10.1016/j.jclepro.2018.08.267
 
+        In addition to the original Matlab code, this implementation
+        also includes pre-balancing to ensure that countries don't
+        export more than they produce and import.
+
         Arguments:
             None
 
         Returns:
             pd.DataFrame: The trade matrix without re-exports.
         """
-        pass
+        # Retain only the countries where we have production data and trade data
+        countries = np.intersect1d(self.trade_matrix.index, self.production_data.index)
+        trade_matrix = self.trade_matrix.loc[countries, countries]
+        production_data = self.production_data.loc[countries]
+        # Make sure this worked
+        assert trade_matrix.shape[0] == production_data.shape[0]
 
+        print("Ignoring countries without production data.")
+ 
+        trade_matrix = self.prebalance(production_data, trade_matrix)
+        production_data, trade_matrix = self.remove_net_zero_countries(
+            production_data,
+            trade_matrix
+        )
+        trade_matrix = self.correct_reexports(production_data, trade_matrix)
 
-    def prebalance(self, vec_prod, mat_trad, precision=10**-3):
-        test = vec_prod + mat_trad.sum(axis=0) - mat_trad.sum(axis=1)
+        # Remove countries with trade which is below the percentile
+        # of the total trade of all countries for this crop
+        trade_matrix = self.remove_above_percentile(trade_matrix, self.percentile)
+        self.trade_matrix = trade_matrix
+        
+    def prebalance(self, production_data, trade_matrix, precision=10**-3):
+        test = production_data + trade_matrix.sum(axis=0) - trade_matrix.sum(axis=1)
         while (test <= -precision).any():
-            sf = (vec_prod + mat_trad.sum(axis=0)) / mat_trad.sum(axis=1)
+            sf = (production_data + trade_matrix.sum(axis=0)) / trade_matrix.sum(axis=1)
             multiplier = ((test < 0) * sf).replace(0, 1)
-            mat_trad = pd.DataFrame(
-                np.diag(multiplier) @ mat_trad.values,
-                index=mat_trad.index,
-                columns=mat_trad.columns,
+            trade_matrix = pd.DataFrame(
+                np.diag(multiplier) @ trade_matrix.values,
+                index=trade_matrix.index,
+                columns=trade_matrix.columns,
             )
-            test = vec_prod + mat_trad.sum(axis=0) - mat_trad.sum(axis=1)
-        return mat_trad
+            test = production_data + trade_matrix.sum(axis=0) - trade_matrix.sum(axis=1)
+        return trade_matrix
 
-
-    def remove_net_zero_countries(self, vec_prod, mat_trad):
-        b_zero_prod = vec_prod == 0
-        b_zero_colsum = mat_trad.sum(axis=0) == 0
-        b_zero_rowsum = mat_trad.sum(axis=1) == 0
+    def remove_net_zero_countries(self, production_data, trade_matrix):
+        b_zero_prod = production_data == 0
+        b_zero_colsum = trade_matrix.sum(axis=0) == 0
+        b_zero_rowsum = trade_matrix.sum(axis=1) == 0
         b_filter = ~(b_zero_prod & b_zero_rowsum & b_zero_colsum)
-        return vec_prod[b_filter], mat_trad.loc[b_filter, b_filter]
+        return production_data[b_filter], trade_matrix.loc[b_filter, b_filter]
 
+    def correct_reexports(self, production_data, trade_matrix):
+        """
+        I know that the variable names here are confusing, but this is a conversion
+        by the original R code from Johanna Hedlung. The variable names are the
+        same as in the R code and we leave them this way, so we can more easily
+        compare the two pieces of code if something goes wrong.
+        """
+        trade_matrix = trade_matrix.T
+        production_data = production_data.fillna(0)
 
-    def correct_reexports(self, vec_prod, mat_trad):
-        mat_trad = mat_trad.T
-        vec_prod = vec_prod.fillna(0)
-
-        x = vec_prod + mat_trad.sum(axis=1)
+        x = production_data + trade_matrix.sum(axis=1)
         y = np.linalg.inv(np.diag(x))
-        A = mat_trad @ y
-        R = np.linalg.inv(np.identity(len(A)) - A) @ np.diag(vec_prod)
-        c = np.diag(y @ (x - mat_trad.sum(axis=0)))
+        A = trade_matrix @ y
+        R = np.linalg.inv(np.identity(len(A)) - A) @ np.diag(production_data)
+        c = np.diag(y @ (x - trade_matrix.sum(axis=0)))
         R = (c @ R).T
         R[~np.isfinite(R)] = 0
         R[R < 0.001] = 0
 
-        return pd.DataFrame(R, index=mat_trad.index, columns=mat_trad.columns)
+        return pd.DataFrame(R, index=trade_matrix.index, columns=trade_matrix.columns)
 
--- tests/test_preprocessing.py
+++ tests/test_preprocessing.py
@@ -46,7 +46,11 @@ def test_returns_correct_dataframe_production():
 def test_extracts_right_data_production():
     result = read_in_raw_production_data()
     # Extract only the relevant data for the production model.
-    result = extract_relevant_production_data(result, ["Maize (corn)", "Wheat", "Rice, paddy (rice milled equivalent)"], year=2018)
+    result = extract_relevant_production_data(result, [
+        "Maize (corn)",
+        "Wheat",
+        "Rice, paddy (rice milled equivalent)"
+    ], year=2018)
     assert isinstance(result, pd.DataFrame)
     # This is the shape of the complete dataset.
     assert result.shape == (362, 5)
