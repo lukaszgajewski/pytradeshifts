Fixed wrong order of prebalancing

tests/test_model.py
@@ -30,6 +30,58 @@ def test_loading_global():
     loading("Global")
 
 
+def removing_countries_with_zeros(region):
+    """
+    Runs the model with prebalancing and compares the results with the
+    results from the R script.
+    """
+    Wheat2018 = PyTradeShifts("Wheat", "Y2018", region=region, testing=True)
+
+    # Load the data
+    Wheat2018.load_data()
+
+    # Remove countries with both zero trade and zero production
+    Wheat2018.remove_net_zero_countries()
+
+    if region == "Global":
+        region = "All_Data"
+
+    # Load the data from the R script
+    wheat2018_from_R = pd.read_csv(
+        "data"
+        + os.sep
+        + "validation_data_from_Hedlung_2022"
+        + os.sep
+        + region
+        + os.sep
+        + "NEW_trade_wheat_2018.csv",
+        index_col=0,
+    )
+
+    # Compare the results
+    assert wheat2018_from_R.shape == Wheat2018.trade_matrix.shape
+    # Comparing the sum this has only be correct to 2 decimal places
+    assert round(wheat2018_from_R.sum().sum(), 2) == round(
+        Wheat2018.trade_matrix.sum().sum(), 2
+    )
+
+
+def test_removing_countries_with_zeros_oceania():
+    """
+    Runs the model with prebalancing and compares the results with the
+    results from the R script.
+    """
+    removing_countries_with_zeros("Oceania")
+
+
+def test_removing_countries_with_zeros_global():
+    """
+    Runs the model with prebalancing and compares the results with the
+    results from the R script.
+    """
+    removing_countries_with_zeros("Global")
+
+
 def prebalancing(region):
     """
     Runs the model with prebalancing and compares the results with the
@@ -39,6 +91,9 @@ def prebalancing(region):
 
     # Load the data
     Wheat2018.load_data()
+
+    # Remove countries with both zero trade and zero production
+    Wheat2018.remove_net_zero_countries()
 
     # Run the prebalancing
     Wheat2018.prebalance()
@@ -81,7 +136,7 @@ def test_prebalancing_global():
     prebalancing("Global")
 
 
-def removing_countries_with_zeros(region):
+def reexport(region):
     """
     Runs the model with prebalancing and compares the results with the
     results from the R script.
@@ -91,56 +146,11 @@ def removing_countries_with_zeros(region):
     # Load the data
     Wheat2018.load_data()
 
-    # Run the prebalancing
-    Wheat2018.prebalance()
-
-    shape_before = Wheat2018.trade_matrix.shape
-
-    # Remove countries with zero trade
+    # Remove countries with zero trade and zero production
     Wheat2018.remove_net_zero_countries()
-    shape_after = Wheat2018.trade_matrix.shape
-
-    # Check if there are still countries with zero trade by
-    # summing over the columns and checking if there are any zeros
-    assert 0 not in Wheat2018.trade_matrix.sum(axis=0).values
-    assert 0 not in Wheat2018.trade_matrix.sum(axis=1).values
-
-    # Check if it worked out
-    assert shape_before[0] > shape_after[0]
-    assert shape_before[1] > shape_after[1]
-
-
-def test_removing_countries_with_zeros_oceania():
-    """
-    Runs the model with prebalancing and compares the results with the
-    results from the R script.
-    """
-    removing_countries_with_zeros("Oceania")
-
-
-def test_removing_countries_with_zeros_global():
-    """
-    Runs the model with prebalancing and compares the results with the
-    results from the R script.
-    """
-    removing_countries_with_zeros("Global")
-
-
-def removing_countries_with_zeros_and_reexport(region):
-    """
-    Runs the model with prebalancing and compares the results with the
-    results from the R script.
-    """
-    Wheat2018 = PyTradeShifts("Wheat", "Y2018", region=region, testing=True)
-
-    # Load the data
-    Wheat2018.load_data()
 
     # Run the prebalancing
     Wheat2018.prebalance()
-
-    # Remove countries with zero trade
-    Wheat2018.remove_net_zero_countries()
 
     # Reexport
     Wheat2018.correct_reexports()
@@ -172,20 +182,20 @@ def removing_countries_with_zeros_and_reexport(region):
     )
 
 
-def test_removing_countries_with_zeros_and_reexport_oceania():
+def test_reexport_oceania():
     """
     Runs the model with prebalancing and compares the results with the
     results from the R script.
     """
-    removing_countries_with_zeros_and_reexport("Oceania")
+    reexport("Oceania")
 
 
-def test_removing_countries_with_zeros_and_reexport_global():
+def test_reexport_global():
     """
     Runs the model with prebalancing and compares the results with the
     results from the R script.
     """
-    removing_countries_with_zeros_and_reexport("Global")
+    reexport("Global")
 
 
 def removing_low_trade_countries(region):
@@ -197,38 +207,18 @@ def removing_low_trade_countries(region):
 
     # Load the data
     Wheat2018.load_data()
-    print("Loaded data")
-    print(np.percentile(
-            Wheat2018.trade_matrix.values[Wheat2018.trade_matrix.values > 0], Wheat2018.percentile * 100
-        ))
+    
+    # Remove countries with zero trade
+    Wheat2018.remove_net_zero_countries()
 
     # Run the prebalancing
     Wheat2018.prebalance()
-    print("Prebalanced")
-    print(np.percentile(
-            Wheat2018.trade_matrix.values[Wheat2018.trade_matrix.values > 0], Wheat2018.percentile * 100
-        ))
-
-    # Remove countries with zero trade
-    Wheat2018.remove_net_zero_countries()
-    print("Removed net zero countries")
-    print(np.percentile(
-            Wheat2018.trade_matrix.values[Wheat2018.trade_matrix.values > 0], 75
-        ))
 
     # Reexport
     Wheat2018.correct_reexports()
-    print("Corrected reexports")
-    print(np.percentile(
-            Wheat2018.trade_matrix.values[Wheat2018.trade_matrix.values > 0], 75
-        ))
 
     # Remove countries with low trade
     Wheat2018.remove_below_percentile()
-    print("Removed below percentile")
-    print(np.percentile(
-            Wheat2018.trade_matrix.values[Wheat2018.trade_matrix.values > 0], 75
-        ))
 
     print(Wheat2018.trade_matrix.shape)
 
@@ -257,4 +247,4 @@ def test_removing_low_trade_countries_global():
 
 
 if __name__ == "__main__":
-    removing_low_trade_countries("Global")
+    reexport("Global")