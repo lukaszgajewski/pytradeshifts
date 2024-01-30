d2ddb594b0cebaee730b6a7fecec257d90593978
Florian Ulrich Jehn <florianjehn@posteo.de>
2024-01-30 08:28:40 +0100

    Trying to skip the tests that need the raw data on Github Acions

--- tests/test_model.py
+++ tests/test_model.py
@@ -3,7 +3,7 @@ import pandas as pd
 import os
 import numpy as np
 from src.preprocessing import rename_countries
-import country_converter as coco
+import pytest
 
 
 def loading(region):
@@ -212,6 +212,7 @@ def reexport(region):
         assert (wheat2018_from_R == trade_matrix_sorted).all(axis=None)
 
 
+@pytest.mark.skipif(os.getenv('GITHUB_ACTIONS') == 'true', reason='Skipping test for Github Actions')
 def test_reexport_oceania():
     """
     Runs the model with prebalancing and compares the results with the
@@ -219,7 +220,7 @@ def test_reexport_oceania():
     """
     reexport("Oceania")
 
-
+@pytest.mark.skipif(os.getenv('GITHUB_ACTIONS') == 'true', reason='Skipping test for Github Actions')
 def test_reexport_global():
     """
     Runs the model with prebalancing and compares the results with the
