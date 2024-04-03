17c8052ba9d23e512e2b3c73c6a31fb0d4b7ebb0
lukaszgajewski <lukaszgajewski@tuta.io>
2024-04-03 13:38:15 +0200

    add combined DS

--- intmodel/src/combine_ds_files.py
+++ intmodel/src/combine_ds_files.py
@@ -1,15 +1,25 @@
 import pandas as pd
 from os import listdir
 from os.path import isfile, join
-from read_nutrition_data import read_nutrition_data
+from functools import reduce
 
-ds_dir = "intmodel/data/prod_trade"
+ds_dir = "intmodel/data/domestic_supply"
 ds_files = [f for f in listdir(ds_dir) if isfile(join(ds_dir, f))]
-items = set([f[: f.find("_")] for f in ds_files])
-nd = read_nutrition_data()
-print(sorted(items.intersection(nd.index)))
 
-# for f in ds_files:
-#     x = pd.read_csv(ds_dir + "/" + f, header=0, names=["Country", "Domestic Supply"])
-#     print(x)
-#     break
+dfs = [
+    pd.read_csv(ds_dir + "/" + f, header=0, names=["Country", f[: f.find("_")]])
+    for f in ds_files
+]
+df = reduce(lambda left, right: pd.merge(left, right, on=["Country"], how="outer"), dfs)
+df = df.fillna(0)
+
+iso3 = pd.read_csv("data/data_raw/iso3.csv")
+iso3.columns = ["iso3", "Country"]
+df = df.merge(iso3)
+df = df.melt(
+    id_vars=["iso3", "Country"],
+    value_vars=[c for c in df.columns if c != "iso3" and c != "Country"],
+)
+df.columns = ["Area Code (ISO3)", "Area", "Item", "Value"]
+
+df.to_csv("intmodel/data/domestic_supply_combined.csv")
