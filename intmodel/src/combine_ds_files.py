81e1853b1d4e28d0828c3f517895bb2bd0da41c9
lukaszgajewski <lukaszgajewski@tuta.io>
2024-04-03 16:34:06 +0200

    fix country names in ds combine script

--- intmodel/src/combine_ds_files.py
+++ intmodel/src/combine_ds_files.py
@@ -2,6 +2,9 @@ import pandas as pd
 from os import listdir
 from os.path import isfile, join
 from functools import reduce
+import country_converter as coco
+
+cc = coco.CountryConverter()
 
 ds_dir = "intmodel/data/domestic_supply"
 ds_files = [f for f in listdir(ds_dir) if isfile(join(ds_dir, f))]
@@ -13,9 +16,8 @@ dfs = [
 df = reduce(lambda left, right: pd.merge(left, right, on=["Country"], how="outer"), dfs)
 df = df.fillna(0)
 
-iso3 = pd.read_csv("data/data_raw/iso3.csv")
-iso3.columns = ["iso3", "Country"]
-df = df.merge(iso3)
+df["iso3"] = cc.pandas_convert(pd.Series(df["Country"]), to="ISO3")
+
 df = df.melt(
     id_vars=["iso3", "Country"],
     value_vars=[c for c in df.columns if c != "iso3" and c != "Country"],
