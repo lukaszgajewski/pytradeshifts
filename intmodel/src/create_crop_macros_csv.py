a9bec3499c0a35fed72cf5ac96da7d2deb30a068
lukaszgajewski <lukaszgajewski@tuta.io>
2024-04-04 13:34:25 +0200

    refactor create crop macros script

--- intmodel/src/create_crop_macros_csv.py
+++ intmodel/src/create_crop_macros_csv.py
@@ -1,209 +1,72 @@
-"""
-This file contains the code for importing crop production macronutrients
-using the production of each crop combined with the nutrition of the crops.
-
-@author: morgan
-"""
-
 import pandas as pd
-import numpy as np
-from import_utilities import ImportUtilities  # TODO, to be changed later probably
 
 
-class CropMacros:
-    def __init__(self):
-        """
-        Initializes the class with necessary constants and imports nutrient and product data.
-        """
-        # Set paths to nutrition data and food production data
-        self.NUTRITION_XLS = (
-            "intmodel/data/ALLFED Food consumption, supplies and balances.xlsx"
-        )
-        # domestic supply
-        self.DS_CSV = "intmodel/data/domestic_supply_combined.csv"
+def import_nutrients_and_products(
+    nutrition_xls, domestic_supply_csv
+) -> tuple[pd.DataFrame, pd.DataFrame]:
+    """
+    Return nutrition and production data from Excel and CSV files respectively.
 
-        # Set conversion factors
-        self.TONS_TO_KG = 1e3
-        self.KCALS_TO_DRY_CALORIC_TONS = 1 / (4000 * 1000)
+    Arguments:
+        None
 
-        # Set daily nutrient requirements per person
-        self.KCALS_PER_PERSON = 2100
-        self.FAT_PER_PERSON = 47
-        self.PROTEIN_PER_PERSON = 53
+    Returns:
+        tuple: a tuple containing two dataframes: products and nutrition
+    """
+    nutrition = pd.read_excel(
+        nutrition_xls,
+        sheet_name="Nutrition data from FAOSTAT",
+        usecols="A:E",
+        skiprows=1,
+    )
+    products = pd.read_csv(domestic_supply_csv, index_col=0)
+    return products, nutrition
 
-        # Import nutrient and product data
-        self.products, self.nutrition = self.import_nutrients_and_products()
 
-    def import_nutrients_and_products(self):
-        """
-        This function imports nutrition and production data from Excel and CSV files respectively.
-        It returns two dataframes: products and nutrition.
+def compute_total_nutrients(products: pd.DataFrame, nutrition: pd.DataFrame):
+    """
+    Return the sum of kcals, fat, and protein for the products passed in.
 
-        Args:
-            self (object): instance of the class
+    Arguments:
+        products (pandas.DataFrame): A DataFrame containing food products and their values.
 
-        Returns:
-            tuple: a tuple containing two dataframes: products and nutrition
-        """
-
-        # Import nutrition data from Excel file
-        # xls = pd.ExcelFile(self.NUTRITION_XLS)
-        nutrition = pd.read_excel(
-            self.NUTRITION_XLS,
-            sheet_name="Nutrition data from FAOSTAT",
-            usecols="A:E",
-            skiprows=1,
-        )
-        # nutrition = pd.read_excel(xls, "Nutrition")[
-        #     ["Item", "Calories", "Protein", "Fat"]
-        # ]
-
-        # Define column names for production data
-        iso3_code = "Area Code (ISO3)"
-        production_col_names = [
-            iso3_code,
-            "Area",
-            # "Element",
-            # "Item Code (FAO)",
-            "Item",
-            # "Unit",
-            "Value",
-        ]
-
-        # Import production data from CSV file
-        products = ImportUtilities.import_csv(
-            self.DS_CSV, production_col_names, iso3_code
-        )
-        # Return the two dataframes
-        return products, nutrition
-
-    def get_nutrients(self, products):
-        """
-        Returns the sum of kcals, fat, and protein for the products passed in.
-
-        Args:
-            products (pandas.DataFrame): A DataFrame containing food products and their values.
-
-        Returns:
-            list: A list containing the sum of kcals, fat, and protein for the products passed in.
-        """
-
-        # Initialize variables to hold the sum of kcals, fat, and protein
-        kcals_sum = 0
-        fat_sum = 0
-        protein_sum = 0
-
-        # For each food product, add to each macronutrient total
-        for index, product in products.iterrows():
-            # Find the particular item in the nutrition DataFrame
-            n = self.nutrition[self.nutrition["Item"] == product["Item"]]
-
-            # If the match could not be found, continue to the next product
-            if len(n) == 0:
-                continue
-
-            # There should never be any duplicate nutrition items
-            assert len(n) == 1
-
-            # Get the kcals, fat, and protein values for the product
-            kcals_per_kg = float(n["Calories"])  # kcals / kg
-            fat_frac = float(n["Fat"])  # fraction by weight
-            protein_frac = float(n["Protein"])  # fraction by weight
-
-            # Production Calories is units tons per year
-            if np.isnan(product["Value"]):
-                tons = 0
-            else:
-                tons = product["Value"]
-
-            # Calculate the dry caloric tons per year
-            kcals = (
-                tons * self.TONS_TO_KG * kcals_per_kg * self.KCALS_TO_DRY_CALORIC_TONS
-            )
-
-            # Convert nutrition Fat and protein from percent by weight to grams
-            fat = tons * fat_frac  # tons per year
-            protein = tons * protein_frac  # tons per year
-
-            # Add the kcals, fat, and protein values to their respective sums
-            kcals_sum += kcals
-            fat_sum += fat
-            protein_sum += protein
-
-        # Return the sum of kcals, fat, and protein for the products passed in
-        return [kcals_sum, fat_sum, protein_sum]
-
-    def get_macros_csv(self):
-        """
-        This function generates a stack of macronutrients that correspond to each country.
-        It loops through all the countries and their corresponding ISO3 codes, and for each country,
-        it calculates the total kcals, fat, and protein produced by all crops in that country.
-        It then adds this information to a numpy array and returns it.
-
-        Returns:
-            numpy.ndarray: A numpy array containing the following columns:
-                - iso3: The ISO3 code of the country
-                - country: The name of the country
-                - crop_kcals: The total kcals produced by all crops in the country
-                - crop_fat: The total fat produced by all crops in the country
-                - crop_protein: The total protein produced by all crops in the country
-        """
-        # Get the ISO3 codes and country names
-        iso3_codes = ImportUtilities.country_codes
-        country_names = ImportUtilities.country_names
-
-        # Create the numpy array with the column names
-        macros_csv = np.array(
-            [
-                "iso3",
-                "country",
-                "crop_kcals",
-                "crop_fat",
-                "crop_protein",
-            ]
-        )
-
-        # Loop through all the countries
-        for i in range(0, len(iso3_codes)):
-            iso3_code = iso3_codes[i]
-            country_name = country_names[i]
-
-            # If the country is not in the products dictionary, skip it
-            if iso3_code not in self.products.keys():
-                print("missing" + iso3_code)
-                continue
-
-            # Get the products for the country
-            products = self.products[iso3_code]
-
-            # Calculate the total kcals, fat, and protein produced by all crops in the country
-            [kcals_sum, fat_sum, protein_sum] = self.get_nutrients(products)
-
-            # Add the information to the numpy array
-            new_layer = [
-                iso3_code,
-                country_name,
-                str(kcals_sum),
-                str(fat_sum),
-                str(protein_sum),
-            ]
-            macros_csv = ImportUtilities.stack_on_list(macros_csv, new_layer)
-
-        # Return the numpy array
-        return macros_csv
+    Returns:
+        list: A list containing the sum of kcals, fat, and protein for the products passed in.
+    """
+    # Merge dataframes
+    df = pd.merge(left=nutrition, right=products, on="Item")
+    # Replace NaNs with zeroes
+    df["Value"].fillna(0, inplace=True)
+    # Convert units
+    df["Value"] = df["Value"]
+    df["Calories"] = df["Calories"] * (1 / 4000)  # kcals to dry caloric kg
+    # Compute nutrients
+    df["crop_kcals"] = df["Calories"] * df["Value"]
+    df["crop_fat"] = df["Fat"] * df["Value"]
+    df["crop_protein"] = df["Protein"] * df["Value"]
+    # Sum it all up
+    df = df.groupby(["Area Code (ISO3)", "Area"]).sum(numeric_only=True)[
+        ["crop_kcals", "crop_fat", "crop_protein"]
+    ]
+    # Format
+    df = df.reset_index()
+    df.columns = [
+        "iso3",
+        "country",
+        "crop_kcals",
+        "crop_fat",
+        "crop_protein",
+    ]
+    return df
 
 
 if __name__ == "__main__":
-    print("importing baseline crop kcals, fat, protein production data...")
-
-    cm = CropMacros()
-
-    macros_csv = cm.get_macros_csv()
-    macros_csv = ImportUtilities.clean_up_eswatini(macros_csv)
-
-    np.savetxt(
-        "intmodel/data/macros_csv.csv",
-        macros_csv,
-        delimiter=",",
-        fmt="%s",
+    products, nutrients = import_nutrients_and_products(
+        "intmodel/data/ALLFED Food consumption, supplies and balances.xlsx",
+        "intmodel/data/domestic_supply_combined.csv",
     )
+    total_nutrients = compute_total_nutrients(products, nutrients)
+    # Fix Eswatini/Swaziland iso3 code
+    total_nutrients["iso3"].replace("SWZ", "SWT", inplace=True)
+    # Save to file
+    total_nutrients.to_csv("intmodel/data/macros_csv.csv", index=False)
