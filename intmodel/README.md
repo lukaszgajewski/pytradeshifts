6c629db4e6c6968d2dddc446180a00242d38d432
lukaszgajewski <lukaszgajewski@tuta.io>
2024-04-05 12:51:41 +0200

    combine all ds files

--- intmodel/README.md
+++ intmodel/README.md
@@ -4,11 +4,13 @@ Considering the amount of data they are ```.gitingored```.
 
 Running preprocessing will produce them (warning: takes several hours [2-3] and a lot of RAM).
 
-Other steps are pretty quick (seconds).
+Other steps are pretty quick (seconds or minutes).
 
-2.5hrs for scenarios, this is FAO food items times number of months times number of countries of operations.
+2-3hrs again for calculating domestic supply for all scenarios, this is the number of FAO food items times number of months times number of countries of operations.
 Some of it is vectorised but not all so it takes a while.
 It ends up being around 20 thousand files that we then also have to combine.
+
+In the output of the domestic supply script not all messages like "Almonds, with shell production/trade or crop_reduction_month_74.csv data not found, skipping." are errors. Almonds, with shell are simply no longer in the FAO data (now it is "in shell"), so such logs need careful considerations.
 
 # TODO
 - write code for generating domestic supply in a scenario (basic functionality is there, only handling file names is left to do)
--- intmodel/src/combine_ds_files.py
+++ intmodel/src/combine_ds_files.py
@@ -3,25 +3,47 @@ from os import listdir
 from os.path import isfile, join
 from functools import reduce
 import country_converter as coco
+from domestic_supply import get_scenarios
+from tqdm import tqdm
 
-cc = coco.CountryConverter()
 
-ds_dir = "intmodel/data/domestic_supply"
-ds_files = [f for f in listdir(ds_dir) if isfile(join(ds_dir, f))]
+def combine_files(ds_dir, ds_files, output_file):
+    dfs = [
+        pd.read_csv(ds_dir + "/" + f, header=0, names=["Country", f[: f.find("_")]])
+        for f in ds_files
+    ]
+    df = reduce(
+        lambda left, right: pd.merge(left, right, on=["Country"], how="outer"), dfs
+    )
+    df = df.fillna(0)
+    cc = coco.CountryConverter()
+    df["iso3"] = cc.pandas_convert(pd.Series(df["Country"]), to="ISO3")
 
-dfs = [
-    pd.read_csv(ds_dir + "/" + f, header=0, names=["Country", f[: f.find("_")]])
-    for f in ds_files
-]
-df = reduce(lambda left, right: pd.merge(left, right, on=["Country"], how="outer"), dfs)
-df = df.fillna(0)
+    df = df.melt(
+        id_vars=["iso3", "Country"],
+        value_vars=[c for c in df.columns if c != "iso3" and c != "Country"],
+    )
+    df.columns = ["Area Code (ISO3)", "Area", "Item", "Value"]
 
-df["iso3"] = cc.pandas_convert(pd.Series(df["Country"]), to="ISO3")
+    df.to_csv(output_file)
 
-df = df.melt(
-    id_vars=["iso3", "Country"],
-    value_vars=[c for c in df.columns if c != "iso3" and c != "Country"],
-)
-df.columns = ["Area Code (ISO3)", "Area", "Item", "Value"]
 
-df.to_csv("intmodel/data/domestic_supply_combined.csv")
+if __name__ == "__main__":
+    ds_dir = "intmodel/data/domestic_supply"
+    print("Compbining no scenario domestic supply.")
+    ds_files = [
+        f
+        for f in listdir(ds_dir)
+        if isfile(join(ds_dir, f)) and "crop_reduction" not in f
+    ]
+    output_file = "intmodel/data/domestic_supply_combined/domestic_supply_combined.csv"
+    combine_files(ds_dir, ds_files, output_file)
+    print("Compbining domestic supply with crop reduction.")
+    scenarios = get_scenarios("intmodel/data/scenario_files")
+    for scenario in tqdm(scenarios):
+        ds_files = [
+            f for f in listdir(ds_dir) if isfile(join(ds_dir, f)) and scenario in f
+        ]
+        combine_files(
+            ds_dir, ds_files, f"intmodel/data/domestic_supply_combined/{scenario}"
+        )
