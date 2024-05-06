becf86b01e6fdd28ccef2387540b57e5720f2034
lukaszgajewski <lukaszgajewski@tuta.io>
2024-05-06 16:15:50 +0200

    add reindex test

--- tests/test_pkl_to_total.py
+++ tests/test_pkl_to_total.py
@@ -3,7 +3,10 @@ import numpy as np
 import pytest
 from src.input_output import load_fao_zip, data
 from src import fao_trade_zip_to_pkl, fao_production_zip_to_pkl
-from src.fao_pkl_to_total_caloric_trade_and_production import compute_calories
+from src.fao_pkl_to_total_caloric_trade_and_production import (
+    compute_calories,
+    reindex_trade_and_production,
+)
 
 
 @pytest.fixture
@@ -42,3 +45,26 @@ def test_compute_calories(oceania_dataset_filtered):
     assert ~production_data.isna().any(axis=None)
     assert production_data.dtypes["Dry Caloric Tonnes"] == np.dtype("float64")
     assert (production_data["Dry Caloric Tonnes"] >= 0).all(axis=None)
+
+
+def test_reindex(oceania_dataset_filtered):
+    trade_data, production_data = oceania_dataset_filtered
+    country_list = ["AUS", "NZL", "PNG", "FJI"]
+    trade_data, production_data = reindex_trade_and_production(
+        trade_data.drop_duplicates(subset=["Reporter ISO3"]).pivot(
+            values=data["input"]["year_flag"],
+            index="Reporter ISO3",
+            columns="Partner ISO3",
+        ),
+        production_data.groupby(["ISO3"]).sum(numeric_only=True).squeeze(),
+        country_list,
+    )
+    assert isinstance(trade_data, pd.DataFrame)
+    assert trade_data.shape == (len(country_list), len(country_list))
+    assert trade_data.columns.equals(trade_data.index)
+    assert trade_data.columns.equals(pd.Index(country_list))
+    assert ~trade_data.isna().any(axis=None)
+    assert isinstance(production_data, pd.Series)
+    assert len(production_data) == len(country_list)
+    assert production_data.index.equals(trade_data.index)
+    assert ~production_data.isna().any(axis=None)
